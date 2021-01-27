e44a79b494050d55407f7c92c8bc6f432eae3674
Yaa Obeng <34660366+Yabayabadoo@users.noreply.github.com>
2021-01-27 18:05:40 -0500

    Update 01_Phuthuma_historic_rollup.R
    Added correction to arrange for calculation NET_NEW, Week_End was not in order.

--- r/Phuthuma/Scripts/01_Phuthuma_historic_rollup.R
+++ r/Phuthuma/Scripts/01_Phuthuma_historic_rollup.R
@@ -36,7 +36,7 @@ df_base<-raw_df %>%
 df_base<-df_base %>% 
   mutate(MechanismID = case_when(PSNU == "ec Buffalo City Metropolitan Municipality" & mon_yr >"2019-12"~ "81902", TRUE ~ MechanismID),
          PrimePartner= case_when(PSNU == "ec Buffalo City Metropolitan Municipality" & mon_yr > "2019-12"~ "Maternal, Adolscent and Child Health (MatCH)",
-                                 TRUE ~ PrimePartner)) %>% 
+                            TRUE ~ PrimePartner)) %>% 
   gather(indicator,val,colnames(select_if(., (is.numeric)))) %>% 
   arrange(Facility, mon_yr, indicator)
 
@@ -68,14 +68,14 @@ df_snapshot <- bind_rows(df_snapshot, df_snapshot1)
 df_netnew<- df_base %>% 
   filter(indicator %in% c("TX_CURR_28")) %>%
   filter(!is.na(val)) %>%  
-  arrange(Facility, mon_yr) %>%
+  arrange(Facility, Week_End, mon_yr) %>%
   group_by(Facility)%>% 
   # mutate(net_new = (val - lag(val, default = 0, order_by = Facility)))
   mutate(net_new = case_when(Siyenza_StartDate == date("2019-08-01") & Week_End < date("2019-08-02") ~ 0,
                              Siyenza_StartDate == date("2019-08-01") & Week_End == date("2019-08-02") ~ 0,
-                             Siyenza_StartDate == date("2019-03-01") & Week_End < date("2019-03-02") ~ 0,
-                             Siyenza_StartDate == Week_End ~ 0,
-                             TRUE ~ (val - lag(val, default = 0, order_by = Facility)))) %>% 
+                            Siyenza_StartDate == date("2019-03-01") & Week_End < date("2019-03-02") ~ 0,
+                            Siyenza_StartDate == Week_End ~ 0,
+                            TRUE ~ (val - lag(val, default = 0, order_by = Facility)))) %>% 
   gather(indicator, val, "net_new") %>% 
   select(-c(Week_Start,Week_End)) %>% 
   group_by(FundingAgency,PrimePartner,MechanismID,SNU1,PSNU,Community,Facility,
@@ -129,15 +129,16 @@ date_ref1<-date_df %>%
                                 mon_yr == "2019-03" ~ date("2019-03-29"),
                                 TRUE ~ nWeek_End))
 
-
+  
 
 # MERGE HISTORICAL DATA WITH WEEK START & END VARIABLES -----------------------------------
 final1<-left_join(final, date_ref1) %>% 
-  filter(mon_yr < "2020-10", !is.na(nWeek_End)) %>%
+filter(mon_yr < "2020-10", !is.na(nWeek_End)) %>%
   rename(Week_End = nWeek_End,
-         Week_Start = nWeek_Start)
-
-
+         Week_Start = nWeek_Start) %>% 
+  mutate(tested)
+  
+  
 
 
 # EXPORT -----------------------------------------------------------------------------
